f26f0bd754704fa3a240d43984b48b392019ce52
Tanner Heffner & Connor Abdelnoor <student@epicodus.com>
2015-03-12 13:35:57 -0700

    Add first test

--- tests/WeekdayTest.php
+++ tests/WeekdayTest.php
@@ -1,10 +1,21 @@
 <?php
 
-    require_once __DIR__."/src/Weekday.php";
+    require_once "src/Weekday.php";
 
     class DateTest extends PHPUnit_Framework_TestCase
     {
-        
+        function test_checkWeekday_today()
+        {
+            //Arrange
+            $test_Weekday = new Date;
+            $input = "12/3/2015";
+
+            //Act
+            $result = $test_Weekday->checkWeekday($input);
+
+            //Assert
+            $this->assertEquals("Thursday", $result);
+        }
     }
 
  ?>
