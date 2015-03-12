798cd48e1e0b8026292a908db42ea250a45605f1
Tanner Heffner & Connor Abdelnoor <student@epicodus.com>
2015-03-12 12:29:20 -0700

    write specs and start test outlines.

---
+++ src/Weekday.php
@@ -0,0 +1,10 @@
+<?php
+    class Date
+    {
+        function checkWeekday($input)
+        {
+
+        }
+    }
+
+ ?>
---
+++ tests/WeekdayTest.php
@@ -0,0 +1,10 @@
+<?php
+
+    require_once __DIR__."/src/Weekday.php";
+
+    class DateTest extends PHPUnit_Framework_TestCase
+    {
+        
+    }
+
+ ?>
